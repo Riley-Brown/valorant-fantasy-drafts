49af00dec8273290fd29ecab286dab622ce3e27e
Riley <riley@riley.gg>
2021-03-14 21:55:37 -0400

    update mongo server file to export createMongoClient function

--- Server/DB/index.js
+++ Server/DB/index.js
@@ -2,9 +2,8 @@ import MongoDB from 'mongodb';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const mongoClient = new MongoDB.MongoClient(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-export default mongoClient;
+export const createMongoClient = () =>
+  new MongoDB.MongoClient(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
