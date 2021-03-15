c4d4f7b371502c4c9ed6f4bad1e75b817d6f0791
Riley <riley@riley.gg>
2021-03-14 22:18:00 -0400

    add upcoming draft page route

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 import { Switch, Route } from 'react-router-dom';
 
 import Player from 'Pages/Player';
-import Leaderboard from 'Pages/Leaderboard';
 import MatchStats from 'Pages/MatchStats';
+import UpcomingDrafts from 'Pages/UpcomingDrafts';
+import UpcomingDraft from 'Pages/UpcomingDraft';
 
 function App() {
   return (
     <>
+      <div className="app-background" />
       <Switch>
         <Route exact path="/player/:playerId" component={Player} />
-        <Route exact path="/" component={Leaderboard} />
         <Route
           exact
           path="/player/:playerId/match/:matchId"
           component={MatchStats}
+        />
+        <Route exact path="/" component={UpcomingDrafts} />
+        <Route
+          exact
+          path="/draft/upcoming/:draftId"
+          component={UpcomingDraft}
         />
       </Switch>
     </>
