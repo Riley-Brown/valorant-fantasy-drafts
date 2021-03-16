da05d872ac9520b6cf3ed5d088029451be09f191
Riley <riley@riley.gg>
2021-03-15 22:49:44 -0400

    update upcoming draft api endpoint, added get closest draft api

--- front-end/src/API/drafts.js
+++ front-end/src/API/drafts.js
@@ -10,12 +10,29 @@ export async function getUpcomingDrafts() {
 }
 
 export async function getUpcomingDraft(draftId) {
-  const res = await fetch(`http://localhost:9999/drafts/upcoming/${draftId}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
+  const res = await fetch(
+    `http://localhost:9999/drafts/upcoming/id/${draftId}`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
     }
-  });
+  );
+
+  return res.json();
+}
+
+export async function getClosestUpcomingDraft() {
+  const res = await fetch(
+    `http://localhost:9999/drafts/upcoming/closest-draft`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  );
 
   return res.json();
 }
