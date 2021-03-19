83cd916f64cad1da5661ae9012e73e63aeec155d
Riley <riley@riley.gg>
2021-03-18 22:09:40 -0400

    update get player matches route to accept body data

--- Server/Components/PlayerMatches/index.js
+++ Server/Components/PlayerMatches/index.js
@@ -1,15 +1,9 @@
 import axios from 'axios';
 
 export async function getPlayerMatches(player) {
-  try {
-    const { data } = await axios.get(
-      `https://api.tracker.gg/api/v2/valorant/standard/matches/riot/${encodeURIComponent(
-        player
-      )}?type=competitive`
-    );
-
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
+  return await axios.get(
+    `https://api.tracker.gg/api/v2/valorant/standard/matches/riot/${encodeURIComponent(
+      player
+    )}?type=competitive`
+  );
 }
--- Server/Routes/PlayerMatches/index.js
+++ Server/Routes/PlayerMatches/index.js
@@ -3,9 +3,14 @@ const router = Router();
 
 import { getPlayerMatches } from '../../Components/PlayerMatches';
 
-router.get('/:playerId', async (req, res) => {
-  const data = await getPlayerMatches(req.params.playerId);
-  res.send(data);
+router.get('/', async (req, res) => {
+  try {
+    const { data } = await getPlayerMatches(req.body.playerId);
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    res.send({ type: 'error', message: 'Player does not exist' });
+  }
 });
 
 export default router;
