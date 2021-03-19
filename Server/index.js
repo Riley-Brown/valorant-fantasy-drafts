114df21a443e914dae0f6c249e1c40887efe4414
Riley <riley@riley.gg>
2021-03-18 22:01:46 -0400

    scores and enter draft server routes

--- Server/index.js
+++ Server/index.js
@@ -10,17 +10,22 @@ import Leaderboard from './Routes/Leaderboard';
 import MatchStats from './Routes/MatchStats';
 import PlayerMatches from './Routes/PlayerMatches';
 import Drafts from './Routes/Drafts';
+import Scores from './Routes/Scores';
+import EnterDraft from './Routes/EnterDraft';
 
 const server = express();
 
 server.use(express.json());
 server.use(cors());
+server.use(express.urlencoded());
 
 // Routes
 server.use('/leaderboard', Leaderboard);
 server.use('/player-matches', PlayerMatches);
 server.use('/match-stats', MatchStats);
 server.use('/drafts', Drafts);
+server.use('/scores', Scores);
+server.use('/drafts/enter-draft', EnterDraft);
 
 const port = process.env.PORT || 9999;
 
