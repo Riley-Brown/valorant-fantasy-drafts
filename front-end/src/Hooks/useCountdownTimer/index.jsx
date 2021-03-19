3a0991228dc4bebd7f5c765cd5b0414778c28211
Riley <riley@riley.gg>
2021-03-18 22:01:27 -0400

    count down timer conditionals for values below 10

--- front-end/src/Hooks/useCountdownTimer/index.jsx
+++ front-end/src/Hooks/useCountdownTimer/index.jsx
@@ -9,7 +9,12 @@ export default function useCountdownTimer({ unixTimestamp }) {
     const minutes = Math.floor((difference / 60) % 60);
     const seconds = Math.floor(difference % 60);
 
-    return { days, hours, minutes, seconds };
+    return {
+      days: days < 10 ? `0${days}` : days,
+      hours: hours < 10 ? `0${hours}` : hours,
+      minutes: minutes < 10 ? `0${minutes}` : minutes,
+      seconds: seconds < 10 ? `0${seconds}` : seconds
+    };
   };
 
   const [timeLeft, setTimeLeft] = useState(() => calculateTimeLeft());
