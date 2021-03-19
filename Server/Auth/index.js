2b19d1bbde75e67dd7629ba57119762433285f10
Riley <riley@riley.gg>
2021-03-19 19:00:03 -0400

    create auth file to handle jwt and bcrypt hashing functionality

---
+++ Server/Auth/index.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+console.log(process.env.JWT_SECRET_KEY);
+
+export const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: '1h'
+  });
+};
+
+export const verifyToken = (token) => {
+  return jwt.verify(token, process.env.JWT_SECRET_KEY);
+};
+
+export const hashPassword = async (password) => {
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return hashedPassword;
+};
+
+export async function validatePasswordHash({ password, hashedPassword }) {
+  return bcrypt.compare(password, hashedPassword);
+}
