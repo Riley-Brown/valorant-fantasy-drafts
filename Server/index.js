c272fdbaf5a86539ab2f18e15f93904bdba0f690
Riley <riley@riley.gg>
2021-03-19 18:59:39 -0400

    import morgan and cookieParser middleware, add login and signup server routes

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -12,12 +14,23 @@ import PlayerMatches from './Routes/PlayerMatches';
 import Drafts from './Routes/Drafts';
 import Scores from './Routes/Scores';
 import EnterDraft from './Routes/EnterDraft';
+import Login from './Routes/Login';
+import Signup from './Routes/Signup';
+
+import { requireAuth } from './Middleware/authMiddleware';
 
 const server = express();
 
+const corsOptions = {
+  credentials: true,
+  origin: ['http://localhost:3000']
+};
+
 server.use(express.json());
-server.use(cors());
-server.use(express.urlencoded());
+server.use(cors(corsOptions));
+server.use(express.urlencoded({ extended: true }));
+server.use(cookieParser());
+server.use(morgan('dev'));
 
 // Routes
 server.use('/leaderboard', Leaderboard);
@@ -25,7 +38,9 @@ server.use('/player-matches', PlayerMatches);
 server.use('/match-stats', MatchStats);
 server.use('/drafts', Drafts);
 server.use('/scores', Scores);
-server.use('/drafts/enter-draft', EnterDraft);
+server.use('/drafts/enter-draft', requireAuth, EnterDraft);
+server.use('/auth/login', Login);
+server.use('/auth/signup', Signup);
 
 const port = process.env.PORT || 9999;
 
