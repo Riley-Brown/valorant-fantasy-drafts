ae22145b08b47d9c68a889d5f1e6881eeaef614d
Riley <riley@riley.gg>
2021-03-20 18:51:34 -0400

    created auth modal component for login and signup forms

---
+++ front-end/src/Components/AuthModal/index.tsx
@@ -0,0 +1,91 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import './AuthModal.scss';
+
+import { setShowAuthModal } from 'Actions/global';
+import { useTypedSelector } from 'Reducers';
+
+import trollLogo from 'Assets/troll-logo.png';
+
+import SignupForm from 'Components/SignupForm';
+import Fade from 'Components/Fade';
+import LoginForm from 'Components/LoginForm';
+import Portal from 'Components/Portal';
+
+export default function AuthModal() {
+  const showAuthModal = useTypedSelector((state) => state.global.showAuthModal);
+
+  const dispatch = useDispatch();
+
+  const [loginType, setLoginType] = useState<'signup' | 'login'>('signup');
+
+  return (
+    <div>
+      <Portal id="auth-modal">
+        <Fade inProp={showAuthModal}>
+          <div
+            id="modal-backdrop"
+            onClick={() => {
+              dispatch(setShowAuthModal(false));
+            }}
+          >
+            <div className="container" onClick={(e) => e.stopPropagation()}>
+              <div className="info">
+                <img className="logo" src={trollLogo} alt="" />
+                {loginType === 'login' ? (
+                  <>
+                    <h1>Login to Dolzy.com</h1>
+                    <p>Login to your account to enter this draft</p>
+                  </>
+                ) : (
+                  <>
+                    <h1>Welcome to Dolzy.com</h1>
+                    <p>Signup for our shitty ass site to enter this draft</p>
+                  </>
+                )}
+              </div>
+              {loginType === 'login' ? (
+                <>
+                  <LoginForm
+                    onLoginSuccess={() => {
+                      dispatch(setShowAuthModal(false));
+                    }}
+                  />
+                  <footer>
+                    <small>
+                      Don't have an account?{' '}
+                      <button
+                        className="btn"
+                        onClick={() => setLoginType('signup')}
+                      >
+                        Signup
+                      </button>
+                    </small>
+                  </footer>
+                </>
+              ) : (
+                <>
+                  <SignupForm
+                    onSignupSuccess={() => dispatch(setShowAuthModal(false))}
+                  />
+                  <footer>
+                    <small>
+                      Already have an account?{' '}
+                      <button
+                        className="btn"
+                        onClick={() => setLoginType('login')}
+                      >
+                        Login
+                      </button>
+                    </small>
+                  </footer>
+                </>
+              )}
+            </div>
+          </div>
+        </Fade>
+      </Portal>
+    </div>
+  );
+}
