8caf9288a21696b660dd9c76f1adc968ef28c3e9
Riley <riley@riley.gg>
2021-03-21 13:23:19 -0400

    update leaderboard to return formatted player data

--- Server/Components/Leaderboard/index.js
+++ Server/Components/Leaderboard/index.js
@@ -6,7 +6,27 @@ export async function getLeaderboard() {
       `https://api.tracker.gg/api/v1/valorant/standard/leaderboards?type=ranked&platform=all&board=default&skip=0&take=100`
     );
 
-    return data;
+    const formattedPlayers = data.data.items.map((player) => {
+      const {
+        avatarUrl,
+        pictureUrl,
+        platformUserHandle
+      } = player.owner.metadata;
+
+      return {
+        avatarUrl,
+        elo: player.value,
+        iconUrl: player.iconUrl,
+        id: player.id,
+        pictureUrl,
+        platformUserHandle,
+        rank: player.rank,
+        userHandleOnly: platformUserHandle.split('#')[0],
+        wins: player.owner.stats[1].value
+      };
+    });
+
+    return formattedPlayers;
   } catch (err) {
     console.log(err);
   }
