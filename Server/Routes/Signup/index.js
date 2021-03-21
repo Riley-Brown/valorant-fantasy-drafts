4417b59ec097e5cd9c2afb8dbfffef296fda52d1
Riley <riley@riley.gg>
2021-03-21 17:40:11 -0400

    add users model to signup route, fix email casing bug

--- Server/Routes/Signup/index.js
+++ Server/Routes/Signup/index.js
@@ -1,69 +1,55 @@
 import { Router } from 'express';
 const router = Router();
 
-import { createMongoClient } from '../../DB';
-
+import { getUsersCollection } from '../../DB/users';
 import { createToken, hashPassword } from '../../Auth';
 
 import { isString } from '../../Helpers';
 
-import { v4 as uuid } from 'uuid';
+import UserModel from '../../Models/User';
 
 router.post('/', async (req, res) => {
   const { email, password } = req.body;
-  console.log({ email, password });
 
   if (!email || !password || !isString(email) || !isString(password)) {
     return res.send({ type: 'error', message: 'Invalid email or password' });
   }
 
-  const mongoClient = createMongoClient();
-  await mongoClient.connect();
+  const { mongoClient, usersCollection } = await getUsersCollection();
 
   try {
-    const usersCollection = mongoClient
-      .db('valorant-draft-db')
-      .collection('users');
-
-    const isUserExist = await usersCollection.findOne({ email });
+    const isUserExist = await usersCollection.findOne({
+      email: email.toLowerCase()
+    });
 
     if (isUserExist) {
-      return res.send({ type: 'error', message: 'User is already registered' });
-    } else {
-      const hashedPassword = await hashPassword(password);
-      const userId = uuid();
-
-      const newUser = {
-        _id: userId,
-        balance: 0,
-        email,
-        password: hashedPassword,
-        signupDate: Math.floor(Date.now() / 1000)
-      };
-
-      const insert = await usersCollection.insertOne(newUser);
-
-      console.log(insert);
-
-      if (insert.insertedCount === 1) {
-        const token = createToken(userId);
-
-        res.cookie('userSession', token, {
-          httpOnly: true,
-          expires: new Date(Date.now() + 90000000)
-        });
-        res.send({ type: 'ok', message: 'User successfully signed up' });
-      } else {
-        res.send({
-          type: 'error',
-          message: 'Something went wrong during signup'
-        });
-      }
+      return res.send({
+        type: 'error',
+        message: 'Email is already registered, email must be unique'
+      });
     }
 
-    // const token = createToken(user.id);
-    // console.log(token);
-    // res.send({ type: 'ok', message: 'User successfully signed up' });
+    const hashedPassword = await hashPassword(password);
+
+    const newUser = UserModel({ email, hashedPassword });
+
+    const insert = await usersCollection.insertOne(newUser);
+
+    if (insert.insertedCount === 1) {
+      const token = createToken(newUser._id);
+
+      res.cookie('userSession', token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 90000000)
+      });
+
+      res.send({ type: 'ok', message: 'User successfully signed up' });
+    } else {
+      res.send({
+        type: 'error',
+        message: 'Something went wrong during signup'
+      });
+    }
   } catch (err) {
     console.log(err);
     res.send({ type: 'error', message: 'Error signing up' });
