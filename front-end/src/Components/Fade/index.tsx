df16bc39da5804a9aa629eef0925b4efb5b542f2
Riley <riley@riley.gg>
2021-03-21 13:16:22 -0400

    fade component event props

--- front-end/src/Components/Fade/index.tsx
+++ front-end/src/Components/Fade/index.tsx
@@ -3,21 +3,38 @@ import { CSSTransition } from 'react-transition-group';
 
 import './Fade.scss';
 
-export default function Fade({
-  inProp,
-  children
-}: {
+interface FadeProps {
   inProp: boolean;
   children: ReactNode;
-}) {
+  onEnter?: () => any;
+  onEntered?: () => any;
+  onExit?: () => any;
+  onExited?: () => any;
+}
+
+const Fade: React.FC<FadeProps> = ({
+  inProp,
+  children,
+  onEnter,
+  onEntered,
+  onExit,
+  onExited
+}: FadeProps) => {
   return (
     <CSSTransition
+      on
       unmountOnExit={true}
       in={inProp}
       timeout={200}
       classNames="fade"
+      onEnter={onEnter}
+      onExit={onExit}
+      onEntered={onEntered}
+      onExited={onExited}
     >
       {children}
     </CSSTransition>
   );
-}
+};
+
+export default Fade;
