276ef6a93904e1e9ccdf200d21e71bccee26b6be
Riley <riley@riley.gg>
2021-03-21 13:21:14 -0400

    remove unneeded selected roster props

--- front-end/src/Pages/ClosestUpcomingDraft/SelectedRoster/index.jsx
+++ front-end/src/Pages/ClosestUpcomingDraft/SelectedRoster/index.jsx
@@ -1,22 +1,15 @@
-import React from 'react';
 import './SelectedRoster.scss';
 
 import SelectedPlayer from './SelectedPlayer';
 
-export default function SelectedRoster({
-  selectedRoster,
-  handleRemoveSelectedPlayer
-}) {
+export default function SelectedRoster({ selectedRoster }) {
   return (
     <div id="selected-roster">
       {Array(5)
         .fill('')
         .map((_, index) =>
           selectedRoster[index] ? (
-            <SelectedPlayer
-              handleRemoveSelectedPlayer={handleRemoveSelectedPlayer}
-              playerData={selectedRoster[index]}
-            />
+            <SelectedPlayer playerData={selectedRoster[index]} />
           ) : (
             <SelectedPlayer empty={true} />
           )
