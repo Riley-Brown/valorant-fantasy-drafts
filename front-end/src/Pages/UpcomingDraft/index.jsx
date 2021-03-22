e5239ddff78035e91710cc2ec453fd9c9b915462
Riley <riley@riley.gg>
2021-03-22 19:55:36 -0400

    remove unneeded draft console log

--- front-end/src/Pages/UpcomingDraft/index.jsx
+++ front-end/src/Pages/UpcomingDraft/index.jsx
@@ -11,7 +11,6 @@ export default function UpcomingDraft() {
 
   useEffect(() => {
     getUpcomingDraft(draftId).then((draftData) => {
-      console.log(draftData);
       setDraftData(draftData);
     });
   }, []);
