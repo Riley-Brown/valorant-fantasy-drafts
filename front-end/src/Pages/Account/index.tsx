e58d6dd216a79df9c68a4cfdd63276745a4705b2
Riley <riley@riley.gg>
2021-03-27 22:46:12 -0400

    account page with basic info and styling

---
+++ front-end/src/Pages/Account/index.tsx
@@ -0,0 +1,66 @@
+import './Account.scss';
+
+import { useTypedSelector } from 'Reducers';
+
+import { formattedInt } from 'Helpers';
+
+import format from 'date-fns/format';
+import fromUnixTime from 'date-fns/fromUnixTime';
+
+import SelectedRoster from 'Pages/ClosestUpcomingDraft/SelectedRoster';
+
+export default function Account() {
+  const account = useTypedSelector((state) => state.account);
+
+  return (
+    <div id="account">
+      <div className="account-info account-section">
+        <h1 className="section-title">Account info</h1>
+        <ul>
+          <div className="wrapper">
+            <li>Email</li>
+            <li>{account.email}</li>
+          </div>
+          <div className="wrapper">
+            <li>Balance</li>
+            <li>${formattedInt(account.balance)}</li>
+          </div>
+          <div className="wrapper">
+            <li>Signup date</li>
+            <li>{format(fromUnixTime(account.signupDate), 'MM/dd/yy')}</li>
+          </div>
+        </ul>
+      </div>
+      <div className="account-drafts account-section">
+        <h1 className="section-title">Draft history</h1>
+        {account.drafts.length > 0 ? (
+          account.drafts.map((draft) => (
+            <div className="draft">
+              <ul>
+                <div className="wrapper">
+                  <li>Enter date</li>
+                  <li>
+                    {format(fromUnixTime(draft.enterDate), 'MM/dd/yy hh:mm a')}
+                  </li>
+                </div>
+                <div className="wrapper">
+                  <li>Entry fee</li>
+                  <li>${formattedInt(draft.entryFee || 0)}</li>
+                </div>
+              </ul>
+
+              {draft.selectedRoster && (
+                <>
+                  <h2 className="selected-roster-title">Selected roster</h2>
+                  <SelectedRoster selectedRoster={draft.selectedRoster} />
+                </>
+              )}
+            </div>
+          ))
+        ) : (
+          <p>No drafts entered yet! Enter your first draft here!</p>
+        )}
+      </div>
+    </div>
+  );
+}
