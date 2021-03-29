f90e38001c94010530cd169b262b3501efacd923
Riley <riley@riley.gg>
2021-03-28 21:38:38 -0400

    added return types for get account api

--- front-end/src/API/account.ts
+++ front-end/src/API/account.ts
@@ -1,6 +1,11 @@
 import { API_ROOT } from './root';
 
-export async function getAccount() {
+import { AccountStateTypes } from 'Reducers/account';
+
+export async function getAccount(): Promise<{
+  type: string;
+  data: AccountStateTypes;
+}> {
   const res = await fetch(`${API_ROOT}/account`, {
     method: 'GET',
     credentials: 'include'
