09146f65027a984360be1b35a1cf1ae829d4328a
Riley <riley@riley.gg>
2021-03-28 21:36:50 -0400

    remove unneeded dotenv conditional

--- Server/DB/index.js
+++ Server/DB/index.js
@@ -1,9 +1,5 @@
 import MongoDB from 'mongodb';
 
-if (process.env.NODE_ENV === 'development') {
-  import('dotenv').then((dotenv) => dotenv.config());
-}
-
 export const createMongoClient = () =>
   new MongoDB.MongoClient(process.env.MONGO_URI, {
     useNewUrlParser: true,
