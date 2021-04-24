265585316e8ea771ee09d95ad39b130e2225a972
Riley Brown <riley@riley.gg>
2021-04-24 16:49:39 -0400

    replace signup form logic with react hook form

--- front-end/src/Components/SignupForm/index.tsx
+++ front-end/src/Components/SignupForm/index.tsx
@@ -1,65 +1,51 @@
-import { useState, FormEvent } from 'react';
-import { signup } from 'API/auth';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 import './SignupForm.scss';
 
-import { useHistory } from 'react-router';
+import { signup } from 'API';
 
 import SpinnerButton from 'Components/SpinnerButton';
-import { useDispatch } from 'react-redux';
-import { setIsAuthed } from 'Actions/global';
-import { getAccount } from 'API/account';
-import { setAccount } from 'Actions/account';
 
 export default function SignupForm({
+  handleGetAccount,
   onSignupSuccess
 }: {
+  handleGetAccount: () => Promise<void>;
   onSignupSuccess: () => any;
 }) {
-  const history = useHistory();
+  const {
+    setError,
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<{ email: string; password: string; password2: string }>();
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [password2, setPassword2] = useState('');
+  console.log(errors);
 
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
 
-  const dispatch = useDispatch();
+  const handleSignup = handleSubmit(async ({ email, password, password2 }) => {
+    console.log(errors, email, password);
 
-  const handleSignup = async (e: FormEvent) => {
-    e.preventDefault();
+    if (password !== password2) {
+      setError('password', { message: 'Passwords do not match' });
+      return;
+    }
 
     setLoading(true);
 
     const signupRes = await signup({ email, password });
 
     if (signupRes.type === 'ok') {
-      dispatch(setIsAuthed(true));
-
-      const account = await getAccount();
-      dispatch(
-        setAccount({
-          balance: account.data.balance,
-          balanceFormatted: `$${account.data.balance * 100}`,
-          drafts: [],
-          email: account.data.email,
-          isAdmin: false,
-          signupDate: account.data.signupDate,
-          stripeCustomerId: undefined,
-          userId: account.data.userId
-        })
-      );
-
+      await handleGetAccount();
       setLoading(false);
-
       onSignupSuccess();
-      // history.push('/');
     } else {
       setLoading(false);
-      setError(signupRes.message);
+      setError('email', { message: signupRes.message });
     }
-  };
+  });
 
   return (
     <div id="signup-form">
@@ -67,36 +53,52 @@ export default function SignupForm({
         <div className="input-wrapper">
           <label htmlFor="email">Email</label>
           <input
-            onChange={({ target }) => setEmail(target.value)}
             type="email"
             id="email"
-            value={email}
+            {...register('email', { required: 'Email is required' })}
           />
-          {error && (
+          {errors.email && (
             <small
               style={{ marginTop: '5px', display: 'block' }}
               className="text-danger"
             >
-              {error}
+              {errors.email.message}
             </small>
           )}
         </div>
         <div className="input-wrapper">
           <label htmlFor="password">Password</label>
           <input
-            onChange={({ target }) => setPassword(target.value)}
             type="password"
             id="password"
-            value={password}
+            {...register('password', { required: 'Password is required' })}
           />
+          {errors.password && (
+            <small
+              style={{ marginTop: '5px', display: 'block' }}
+              className="text-danger"
+            >
+              {errors.password.message}
+            </small>
+          )}
         </div>
         <div className="input-wrapper">
           <label htmlFor="password2">Confirm password</label>
           <input
-            onChange={({ target }) => setPassword2(target.value)}
+            {...register('password2', {
+              required: 'Confirm password is required'
+            })}
             type="password"
             id="password2"
           />
+          {errors.password2 && (
+            <small
+              style={{ marginTop: '5px', display: 'block' }}
+              className="text-danger"
+            >
+              {errors.password2.message}
+            </small>
+          )}
         </div>
         <SpinnerButton
           spinnerProps={{ style: { width: '1.5rem', height: '1.5rem' } }}
