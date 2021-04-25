e6dbcba27a51bc448f7210db01b4ef6fa7aad7bd
Riley Brown <riley@riley.gg>
2021-04-25 00:59:59 -0400

    added logic to calculate draft scores in queue, added events endpoint for frontend on queue completed event

--- Server/Routes/Drafts/index.js
+++ Server/Routes/Drafts/index.js
@@ -1,4 +1,8 @@
 import { Router } from 'express';
+import Queue from 'bull';
+
+import differenceInMinutes from 'date-fns/differenceInMinutes';
+
 const router = Router();
 
 import { requireAdminAuth } from '../../Middleware/authMiddleware';
@@ -11,6 +15,22 @@ import {
   getClosestUpcomingDraft,
   calcDraftScores
 } from '../../Components/Drafts';
+import { fromUnixTime } from 'date-fns';
+
+const scoreQueue = new Queue('calc scores');
+
+scoreQueue.process('calcScore', async (job, done) => {
+  console.log(job);
+
+  try {
+    const scores = await calcDraftScores(job.id);
+    job.update(null);
+    done(null, scores);
+  } catch (err) {
+    job.remove();
+    console.log(err);
+  }
+});
 
 router.post('/create-draft', requireAdminAuth, async (req, res) => {
   const { startDate, endDate, entryFee } = req.body;
@@ -94,19 +114,87 @@ router.get('/upcoming/closest-draft', async (req, res) => {
   }
 });
 
-router.post('/calc-draft-scores', async (req, res) => {
-  try {
-    const scores = await calcDraftScores(req.body.draftId);
-    console.log(scores);
+router.get('/scores/:draftId', async (req, res) => {
+  const { draftId } = req.params;
 
-    res.send(scores);
-  } catch (err) {
-    console.log(err);
-    res.send({
-      type: 'calcScoresError',
-      message: err.message
-    });
+  // await scoreQueue.clean(100);
+  // return;
+
+  // console.log(scoreQueue);
+  // // res.json(scoreQueue);
+  // return;
+
+  console.log(draftId);
+  // await scoreQueue.removeJobs(draftId);
+
+  const job = await scoreQueue.getJob(draftId);
+  console.log(job);
+
+  if (!job) {
+    console.log('no job, added to queue');
+    await scoreQueue.add('calcScore', null, { jobId: draftId });
+    return res.json({ status: 'active', message: 'Started score calc queue' });
   }
+
+  const { finishedOn, processedOn } = job;
+
+  const jobStatus = await job.getState();
+
+  if (jobStatus === 'completed') {
+    const lastUpdated = Math.floor((finishedOn || processedOn) / 1000);
+
+    const minutesSinceCompleted = differenceInMinutes(
+      new Date(),
+      fromUnixTime(lastUpdated)
+    );
+
+    console.log(minutesSinceCompleted);
+
+    // Queue up new job if prev older than 5 minutes
+    if (minutesSinceCompleted > 5) {
+      const prevData = job.returnvalue;
+      await job.remove();
+      await scoreQueue.add('calcScore', prevData, {
+        jobId: draftId
+      });
+
+      return res.json({
+        status: 'active',
+        lastUpdated,
+        data: prevData,
+        message: 'Updating scores inprogress'
+      });
+    }
+  }
+
+  console.log('returning existing cached scores');
+  res.json({
+    status: jobStatus,
+    lastUpdated: Math.floor((finishedOn || processedOn) / 1000),
+    data: job.returnvalue || job.data
+  });
+});
+
+router.get('/scores/:draftId/events', async (req, res) => {
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Connection', 'keep-alive');
+  res.flushHeaders(); // flush the headers to establish SSE with client
+
+  scoreQueue.on('completed', (job) => {
+    res.write(
+      `data: ${JSON.stringify({
+        ...job.returnvalue,
+        lastUpdated: Math.floor(job.finishedOn / 1000)
+      })}\n\n`
+    );
+  });
+
+  res.on('close', () => {
+    scoreQueue.removeAllListeners();
+    res.end();
+  });
 });
 
 export default router;
