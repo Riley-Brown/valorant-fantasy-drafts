13cca0cb86782774fec5e698062ac906d9ca8bc3
Riley Brown <riley@riley.gg>
2021-05-08 18:14:51 -0400

    return display name in get account API, added update account route to allow updating display name

--- Server/Routes/Account/index.js
+++ Server/Routes/Account/index.js
@@ -15,6 +15,41 @@ router.get('/', async (req, res) => {
       type: 'ok',
       data: {
         balance: userAccount.balance,
+        displayName: userAccount.displayName,
+        drafts: (userAccount.drafts || []).reverse(),
+        email: userAccount.email,
+        isAdmin: userAccount.isAdmin,
+        signupDate: userAccount.signupDate,
+        stripeCustomerId: userAccount.stripeCustomerId
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    res.send({ type: 'error', message: 'error getting account' });
+  } finally {
+    await mongoClient.close();
+  }
+});
+
+router.put('/update', async (req, res) => {
+  const { usersCollection, mongoClient } = await getUsersCollection();
+
+  const { id: userId } = res.locals.userTokenObject;
+
+  try {
+    const updateUser = await usersCollection.findOneAndUpdate(
+      { _id: userId },
+      { $set: { displayName: req.body.displayName } },
+      { returnOriginal: false }
+    );
+
+    console.log(updateUser);
+
+    res.send({
+      type: 'ok',
+      data: {
+        balance: userAccount.balance,
+        displayName: userAccount.displayName,
         drafts: (userAccount.drafts || []).reverse(),
         email: userAccount.email,
         isAdmin: userAccount.isAdmin,
