8603b291f4bc42468fef491546f10188fbedef82
Riley Brown <riley@riley.gg>
2021-05-08 21:28:43 -0400

    add display name to User model

--- Server/Models/User.js
+++ Server/Models/User.js
@@ -1,12 +1,13 @@
 import { v4 as uuid } from 'uuid';
 
-export default function UserModel({ email, hashedPassword }) {
+export default function UserModel({ displayName, email, hashedPassword }) {
   const signupDate = Math.floor(Date.now() / 1000);
   const userId = uuid();
 
   return {
     _id: userId,
     balance: 500,
+    displayName,
     email: email.toLowerCase(),
     password: hashedPassword,
     signupDate
