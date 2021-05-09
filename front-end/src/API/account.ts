669cda40df8343c55dc2a1dd78d8e85697bae2d0
Riley Brown <riley@riley.gg>
2021-05-08 23:30:12 -0400

    update account API

--- front-end/src/API/account.ts
+++ front-end/src/API/account.ts
@@ -13,3 +13,21 @@ export async function getAccount(): Promise<{
 
   return res.json();
 }
+
+export async function updateAccount(data: {
+  displayName: string;
+}): Promise<{
+  type: string;
+  data: AccountStateTypes;
+}> {
+  const res = await fetch(`${API_ROOT}/account/update`, {
+    method: 'PUT',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+
+  return res.json();
+}
