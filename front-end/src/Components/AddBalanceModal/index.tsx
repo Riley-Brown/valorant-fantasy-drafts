a94be8619b1d2a7239e084851b01f2f825c6fb53
Riley Brown <riley@riley.gg>
2021-05-25 16:01:12 -0400

    replace repeating add balance and auth modal logic with modal component

--- front-end/src/Components/AddBalanceModal/index.tsx
+++ front-end/src/Components/AddBalanceModal/index.tsx
@@ -13,8 +13,7 @@ import { useToasts } from 'react-toast-notifications';
 import { formatPrice } from 'Helpers';
 
 import SpinnerButton from 'Components/SpinnerButton';
-import Fade from 'Components/Fade';
-import Portal from 'Components/Portal';
+import Modal from 'Components/Modal';
 
 export default function AddBalanceModal() {
   const dispatch = useDispatch();
@@ -66,58 +65,53 @@ export default function AddBalanceModal() {
 
   return (
     <>
-      <Portal id="add-balance-modal">
-        <Fade inProp={show}>
-          <div
-            className="modal-backdrop"
-            onClick={() => dispatch(setShowBalanceModal(false))}
-          >
-            <div className="container" onClick={(e) => e.stopPropagation()}>
-              <h1>Add balance to your account</h1>
-              <form onSubmit={onSubmit}>
-                <div className="input-wrapper">
-                  <label htmlFor="add-balance">Amount</label>
-                  <input
-                    id="add-balance"
-                    type="number"
-                    placeholder="5.00"
-                    pattern="[0-9]*"
-                    inputMode="numeric"
-                    name="addBalance"
-                    value={chargeAmount}
-                    onChange={(e) => setChargeAmount(e.target.value)}
-                    onBlur={(e) => {
-                      setChargeAmount(formatPrice(e.target.value));
-                    }}
-                  />
-                </div>
-                <small>
-                  By clicking submit you agree to charge {chargeAmount} USD to
-                  your {account.payment?.cardBrand} card ending in{' '}
-                  {account.payment?.cardLast4}
-                </small>
-                <div className="buttons-wrapper">
-                  <button
-                    onClick={() => dispatch(setShowBalanceModal(false))}
-                    className="btn"
-                    type="button"
-                  >
-                    Cancel
-                  </button>
-                  <SpinnerButton
-                    disabled={!chargeAmount}
-                    loading={loading}
-                    className="btn submit-btn"
-                    type="submit"
-                  >
-                    Add balance
-                  </SpinnerButton>
-                </div>
-              </form>
-            </div>
+      <Modal
+        id="add-balance-modal"
+        show={show}
+        onHide={() => dispatch(setShowBalanceModal(false))}
+      >
+        <h1>Add balance to your account</h1>
+        <form onSubmit={onSubmit}>
+          <div className="input-wrapper">
+            <label htmlFor="add-balance">Amount</label>
+            <input
+              id="add-balance"
+              type="number"
+              placeholder="5.00"
+              pattern="[0-9]*"
+              inputMode="numeric"
+              name="addBalance"
+              value={chargeAmount}
+              onChange={(e) => setChargeAmount(e.target.value)}
+              onBlur={(e) => {
+                setChargeAmount(formatPrice(e.target.value));
+              }}
+            />
           </div>
-        </Fade>
-      </Portal>
+          <small>
+            By clicking submit you agree to charge {chargeAmount} USD to your{' '}
+            {account.payment?.cardBrand} card ending in{' '}
+            {account.payment?.cardLast4}
+          </small>
+          <div className="buttons-wrapper">
+            <button
+              onClick={() => dispatch(setShowBalanceModal(false))}
+              className="btn"
+              type="button"
+            >
+              Cancel
+            </button>
+            <SpinnerButton
+              disabled={!chargeAmount}
+              loading={loading}
+              className="btn submit-btn"
+              type="submit"
+            >
+              Add balance
+            </SpinnerButton>
+          </div>
+        </form>
+      </Modal>
     </>
   );
 }
--- front-end/src/Components/AuthModal/index.tsx
+++ front-end/src/Components/AuthModal/index.tsx
@@ -8,10 +8,8 @@ import { useTypedSelector } from 'Reducers';
 import placeholderLogo from 'Assets/placeholder-logo.png';
 
 import SignupForm from 'Components/SignupForm';
-import Fade from 'Components/Fade';
 import LoginForm from 'Components/LoginForm';
-import Portal from 'Components/Portal';
-import { useEffect } from 'react';
+import Modal from 'Components/Modal';
 
 export default function AuthModal({
   handleGetAccount
@@ -20,94 +18,71 @@ export default function AuthModal({
 }) {
   const showAuthModal = useTypedSelector((state) => state.global.showAuthModal);
 
-  useEffect(() => {
-    if (showAuthModal) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = '';
-    }
-
-    return () => {
-      document.body.style.overflow = '';
-    };
-  }, [showAuthModal]);
-
   const dispatch = useDispatch();
 
   const loginType = useTypedSelector((state) => state.global.loginType);
 
   return (
     <div>
-      <Portal id="auth-modal">
-        <Fade inProp={showAuthModal}>
-          <div
-            id="modal-backdrop"
-            onClick={() => {
-              dispatch(setShowAuthModal(false));
-            }}
-          >
-            <div className="container" onClick={(e) => e.stopPropagation()}>
-              <div className="info">
-                <img
-                  className="logo"
-                  src={placeholderLogo}
-                  alt="Placeholder logo"
-                />
-                {loginType === 'login' ? (
-                  <>
-                    <h1>Login to test.com</h1>
-                    <p>Login to your account to enter this draft</p>
-                  </>
-                ) : (
-                  <>
-                    <h1>Welcome to test.com</h1>
-                    <p>Signup for our test.com to enter this draft</p>
-                  </>
-                )}
-              </div>
-              {loginType === 'login' ? (
-                <>
-                  <LoginForm
-                    handleGetAccount={handleGetAccount}
-                    onLoginSuccess={() => {
-                      dispatch(setShowAuthModal(false));
-                    }}
-                  />
-                  <footer>
-                    <small>
-                      Don't have an account?{' '}
-                      <button
-                        className="btn"
-                        onClick={() => dispatch(setAuthLoginType('signup'))}
-                      >
-                        Signup
-                      </button>
-                    </small>
-                  </footer>
-                </>
-              ) : (
-                <>
-                  <SignupForm
-                    handleGetAccount={handleGetAccount}
-                    onSignupSuccess={() => dispatch(setShowAuthModal(false))}
-                  />
-                  <footer>
-                    <small>
-                      Already have an account?{' '}
-                      <button
-                        className="btn"
-                        onClick={() => dispatch(setAuthLoginType('login'))}
-                      >
-                        Login
-                      </button>
-                    </small>
-                  </footer>
-                </>
-              )}
-            </div>
-          </div>
-        </Fade>
-      </Portal>
+      <Modal
+        id="auth-modal"
+        show={showAuthModal}
+        onHide={() => dispatch(setShowAuthModal(false))}
+      >
+        <div className="info">
+          <img className="logo" src={placeholderLogo} alt="Placeholder logo" />
+          {loginType === 'login' ? (
+            <>
+              <h1>Login to test.com</h1>
+              <p>Login to your account to enter this draft</p>
+            </>
+          ) : (
+            <>
+              <h1>Welcome to test.com</h1>
+              <p>Signup for our test.com to enter this draft</p>
+            </>
+          )}
+        </div>
+        {loginType === 'login' ? (
+          <>
+            <LoginForm
+              handleGetAccount={handleGetAccount}
+              onLoginSuccess={() => {
+                dispatch(setShowAuthModal(false));
+              }}
+            />
+            <footer>
+              <small>
+                Don't have an account?{' '}
+                <button
+                  className="btn"
+                  onClick={() => dispatch(setAuthLoginType('signup'))}
+                >
+                  Signup
+                </button>
+              </small>
+            </footer>
+          </>
+        ) : (
+          <>
+            <SignupForm
+              handleGetAccount={handleGetAccount}
+              onSignupSuccess={() => dispatch(setShowAuthModal(false))}
+            />
+            <footer>
+              <small>
+                Already have an account?{' '}
+                <button
+                  className="btn"
+                  onClick={() => dispatch(setAuthLoginType('login'))}
+                >
+                  Login
+                </button>
+              </small>
+            </footer>
+          </>
+        )}
+      </Modal>
     </div>
   );
 }
