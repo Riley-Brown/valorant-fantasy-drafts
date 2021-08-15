049f78c743977410ea2acf8f54ec0c005fe36162
Riley Brown <riley@riley.gg>
2021-08-15 11:48:10 -0400

    helper function to map top player IDs to twitch channel information

--- Server/Helpers/index.js
+++ Server/Helpers/index.js
@@ -1,3 +1,369 @@
 export function isString(string) {
   return typeof string === 'string';
 }
+
+export const playersLiveStreams = {
+  'c9 poiz#000': {
+    channelName: 'poizfps',
+    fullUrl: 'https://www.twitch.tv/poizfps'
+  },
+  'zt0l#lotz': {
+    channelName: 'zt0l',
+    fullUrl: 'https://www.twitch.tv/zt0l'
+  },
+  'sq crunchy#cm360': {
+    channelName: 'crunchy_au',
+    fullUrl: 'https://www.twitch.tv/crunchy_au'
+  },
+  't1 curry#0000': {
+    channelName: 'curry',
+    fullUrl: 'https://www.twitch.tv/curry'
+  },
+  'ghost koala#miku': {
+    channelName: 'koalanoob',
+    fullUrl: 'https://www.twitch.tv/koalanoob'
+  },
+  'v1 wippie#nori': {
+    channelName: 'wippiecs',
+    fullUrl: 'https://www.twitch.tv/wippiecs'
+  },
+  'fives#2008': {
+    channelName: 'fives_fn',
+    fullUrl: 'https://www.twitch.tv/fives_fn'
+  },
+  't1 dawn#24k': {
+    channelName: 'ddawn',
+    fullUrl: 'https://www.twitch.tv/ddawn'
+  },
+  'faze babyj#halo': {
+    channelName: 'babyj',
+    fullUrl: 'https://www.twitch.tv/babyj'
+  },
+  'nv yay#yay': {
+    channelName: 'yayster',
+    fullUrl: 'https://www.twitch.tv/yayster'
+  },
+  'geng shawn#12590': {
+    channelName: 'shawn12590',
+    fullUrl: 'https://www.twitch.tv/shawn12590'
+  },
+  'campion#esh': {
+    channelName: 'campion_tv',
+    fullUrl: 'https://www.twitch.tv/campion_tv'
+  },
+  'xset zekken#5193': {
+    channelName: 'zekken',
+    fullUrl: 'https://www.twitch.tv/zekken'
+  },
+  'geeza#skins': {
+    channelName: 'geeza',
+    fullUrl: 'https://www.twitch.tv/geeza'
+  },
+  '100t asuna#1111': {
+    channelName: 'asunaweeb',
+    fullUrl: 'https://www.twitch.tv/asunaweeb'
+  },
+  'sen sick#xdddd': {
+    channelName: 'sick_cs',
+    fullUrl: 'https://www.twitch.tv/sick_cs'
+  },
+  'soar cryo#sadge': {
+    channelName: 'cryocells',
+    fullUrl: 'https://www.twitch.tv/cryocells'
+  },
+  'zaff#805': { channelName: null, fullUrl: null },
+  'rara#nori': { channelName: 'rara', fullUrl: 'https://www.twitch.tv/rara' },
+  'sen shahzam#huh': {
+    channelName: 'shahzam',
+    fullUrl: 'https://www.twitch.tv/shahzam'
+  },
+  'faze rawkus#1337': {
+    channelName: 'itsrawkus',
+    fullUrl: 'https://www.twitch.tv/itsrawkus'
+  },
+  'sq dizzylife#9589': {
+    channelName: 'dizzylife',
+    fullUrl: 'https://www.twitch.tv/dizzylife'
+  },
+  'harmful#angel': {
+    channelName: 'harmful',
+    fullUrl: 'https://www.twitch.tv/harmful'
+  },
+  'ttv pr0phie#sub2': {
+    channelName: 'pr0phie',
+    fullUrl: 'https://www.twitch.tv/pr0phie'
+  },
+  'xset bcj#5ft2': {
+    channelName: 'thebcj',
+    fullUrl: 'https://www.twitch.tv/thebcj'
+  },
+  'nrg s0m#nrg': {
+    channelName: 's0mcs',
+    fullUrl: 'https://www.twitch.tv/s0mcs'
+  },
+  'johnqt#1337': {
+    channelName: 'johnqtcs',
+    fullUrl: 'https://www.twitch.tv/johnqtcs'
+  },
+  'euroh#val': { channelName: null, fullUrl: null },
+  'imt rossy x1#sav': {
+    channelName: 'rossyx1',
+    fullUrl: 'https://www.twitch.tv/rossyx1'
+  },
+  'instinct#5341': {
+    channelName: 'instinctmm',
+    fullUrl: 'https://www.twitch.tv/instinctmm'
+  },
+  'nv victor#777': {
+    channelName: 'victor',
+    fullUrl: 'https://www.twitch.tv/victor'
+  },
+  'playboi joe#jvn': {
+    channelName: 'playboijoeval',
+    fullUrl: 'https://www.twitch.tv/playboijoeval'
+  },
+  'xset thwifo#2628': {
+    channelName: 'thwifo',
+    fullUrl: 'https://www.twitch.tv/thwifo'
+  },
+  'sharky#nba': {
+    channelName: 'sharky',
+    fullUrl: 'https://www.twitch.tv/sharky'
+  },
+  'sym#xoxo': { channelName: null, fullUrl: null },
+  'cain#nate': { channelName: null, fullUrl: null },
+  'nrg eeiu#1945': {
+    channelName: '777eeiu',
+    fullUrl: 'https://www.twitch.tv/777eeiu'
+  },
+  'geng mkael#bot': {
+    channelName: 'mkaelcs',
+    fullUrl: 'https://www.twitch.tv/mkaelcs'
+  },
+  'leak#1641': { channelName: null, fullUrl: null },
+  'twitchemilshe1n#euguy': {
+    channelName: 'emilshe1n',
+    fullUrl: 'https://www.twitch.tv/emilshe1n'
+  },
+  'sen dapr#1of1': {
+    channelName: 'dapr',
+    fullUrl: 'https://www.twitch.tv/dapr'
+  },
+  'kuaresma#snpr': {
+    channelName: null,
+    fullUrl: null
+  },
+  'nc habib#000': {
+    channelName: 'Habibval',
+    fullUrl: 'https://www.twitch.tv/Habibval'
+  },
+  'sen zombs#naysh': {
+    channelName: 'zombs',
+    fullUrl: 'https://www.twitch.tv/zombs'
+  },
+  'lg dazzle#708': {
+    channelName: 'dazzLe',
+    fullUrl: 'https://www.twitch.tv/dazzLe'
+  },
+  'sen sinatraa#idc': {
+    channelName: 'sinatraa',
+    fullUrl: 'https://www.twitch.tv/sinatraa'
+  },
+  'nrg tex#x1mob': {
+    channelName: 'texerino',
+    fullUrl: 'https://www.twitch.tv/texerino'
+  },
+  'lg thief#sof': {
+    channelName: 'thi9f',
+    fullUrl: 'https://www.twitch.tv/thi9f'
+  },
+  'nillyaz#sosa': {
+    channelName: 'nillyaz',
+    fullUrl: 'https://www.twitch.tv/nillyaz'
+  },
+  'xset dephh#xset': {
+    channelName: 'dephh',
+    fullUrl: 'https://www.twitch.tv/dephh'
+  },
+  'freakazoid#gtg': {
+    channelName: 'freakazoid',
+    fullUrl: 'https://www.twitch.tv/freakazoid'
+  },
+  'eg branted#evil': {
+    channelName: 'branted',
+    fullUrl: 'https://www.twitch.tv/branted'
+  },
+  'c9 vanity#xiter': {
+    channelName: 'vanity',
+    fullUrl: 'https://www.twitch.tv/vanity'
+  },
+  'srn neon#000': {
+    channelName: null,
+    fullUrl: null
+  },
+  'soar zander#swag': {
+    channelName: 'zanderfps',
+    fullUrl: 'https://www.twitch.tv/zanderfps'
+  },
+  'c0m#77777': {
+    channelName: 'c0mfps',
+    fullUrl: 'https://www.twitch.tv/c0mfps'
+  },
+  'diceyzx#quan': {
+    channelName: 'diceyzx',
+    fullUrl: 'https://www.twitch.tv/diceyzx'
+  },
+  'dravanra#ttv': {
+    channelName: 'dravanraval',
+    fullUrl: 'https://www.twitch.tv/dravanraval'
+  },
+  'zach attack#6921': {
+    channelName: null,
+    fullUrl: null
+  },
+  'tsm leviathan#2001': {
+    channelName: 'leviathanag_',
+    fullUrl: 'https://www.twitch.tv/leviathanag_'
+  },
+  'faze corey#1337': {
+    channelName: 'corey',
+    fullUrl: 'https://www.twitch.tv/corey'
+  },
+  'jonaaa6#tiff': {
+    channelName: 'jonaaa6',
+    fullUrl: 'https://www.twitch.tv/jonaaa6'
+  },
+  'gabersenn#lnkp': {
+    channelName: 'gabersenn',
+    fullUrl: 'https://www.twitch.tv/gabersenn'
+  },
+  'bearkun#4451': {
+    channelName: 'bearkunv',
+    fullUrl: 'https://www.twitch.tv/bearkunv'
+  },
+  'jasonr24#9498': {
+    channelName: 'jasonr',
+    fullUrl: 'https://www.twitch.tv/jasonr'
+  },
+  'ok thoomer#okt': {
+    channelName: 'okthoomer',
+    fullUrl: 'https://www.twitch.tv/okthoomer'
+  },
+  'nrg wedid#deez': {
+    channelName: 'wedidvalorant',
+    fullUrl: 'https://www.twitch.tv/wedidvalorant'
+  },
+  'player1#ggez': {
+    channelName: 'playerr',
+    fullUrl: 'https://www.twitch.tv/playerr'
+  },
+  'glorinsz#milf': {
+    channelName: 'glorinsz',
+    fullUrl: 'https://www.twitch.tv/glorinsz'
+  },
+  'lear#kirin': {
+    channelName: 'learval',
+    fullUrl: 'https://www.twitch.tv/learval'
+  },
+  'cheatcode#aimer': {
+    channelName: 'cheatcodeVAL',
+    fullUrl: 'https://www.twitch.tv/cheatcodeVAL'
+  },
+  'miniature1#ttv': {
+    channelName: 'miniature1',
+    fullUrl: 'https://www.twitch.tv/miniature1'
+  },
+  'brawk#420': {
+    channelName: 'brawku',
+    fullUrl: 'https://www.twitch.tv/brawku'
+  },
+  'hate#xxx': { channelName: null, fullUrl: null },
+  'jawgemo#mor': {
+    channelName: 'jawgemo',
+    fullUrl: 'https://www.twitch.tv/jawgemo'
+  },
+  'tsm bang#0000': {
+    channelName: 'bangval_',
+    fullUrl: 'https://www.twitch.tv/bangval_'
+  },
+  'ezire#000': {
+    channelName: 'ezirej',
+    fullUrl: 'https://www.twitch.tv/ezirej'
+  },
+  'tsm wardell#8266': {
+    channelName: 'wardell',
+    fullUrl: 'https://www.twitch.tv/wardell'
+  },
+  'bbg will#000': {
+    channelName: null,
+    fullUrl: null
+  },
+  'lanjok#111': {
+    channelName: 'lanjok',
+    fullUrl: 'https://www.twitch.tv/lanjok'
+  },
+  'yungyaz#iran': {
+    channelName: null,
+    fullUrl: null
+  },
+  'c9 xeppaa#xiter': {
+    channelName: 'xeppaa',
+    fullUrl: 'https://www.twitch.tv/xeppaa'
+  },
+  'vora#000': {
+    channelName: 'vora99',
+    fullUrl: 'https://www.twitch.tv/vora99'
+  },
+  'contii#kekw': {
+    channelName: 'contii',
+    fullUrl: 'https://www.twitch.tv/contii'
+  },
+  'laski#9011': {
+    channelName: 'laski',
+    fullUrl: 'https://www.twitch.tv/laski'
+  },
+  'codey#na1': { channelName: 'codey', fullUrl: 'https://www.twitch.tv/codey' },
+  'seven#kun': { channelName: null, fullUrl: null },
+  'zexrow#na2': {
+    channelName: 'tsm_zexrow',
+    fullUrl: 'https://www.twitch.tv/tsm_zexrow'
+  },
+  'eccentrik#9658': {
+    channelName: 'eccentrik',
+    fullUrl: 'https://www.twitch.tv/eccentrik'
+  },
+  'rng jerk#phiz': {
+    channelName: 'jerktbe',
+    fullUrl: 'https://www.twitch.tv/jerktbe'
+  },
+  'big m0nstr#iwnl': {
+    channelName: null,
+    fullUrl: null
+  },
+  'vulcan#stk': {
+    channelName: 'vulcanstk',
+    fullUrl: 'https://www.twitch.tv/vulcanstk'
+  },
+  'divize jovahnii#3189': {
+    channelName: 'jovahnii',
+    fullUrl: 'https://www.twitch.tv/jovahnii'
+  },
+  'srn aiko#kamu': {
+    channelName: null,
+    fullUrl: null
+  },
+  'chakaron#5494': {
+    channelName: 'deriqq',
+    fullUrl: 'https://www.twitch.tv/deriqq'
+  },
+  'bbg bjor#ligma': {
+    channelName: 'bjorlulu',
+    fullUrl: 'https://www.twitch.tv/bjorlulu'
+  },
+  'demon hours#2007': {
+    channelName: null,
+    fullUrl: null
+  },
+  'tigg#007': { channelName: 'tigg', fullUrl: 'https://www.twitch.tv/tigg' },
+  '12pk#xanax': { channelName: null, fullUrl: null }
+};
