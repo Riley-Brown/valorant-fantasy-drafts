f3d189ac06baa0f594f1b20e02438040c7095371
Riley Brown <riley@riley.gg>
2021-08-15 11:47:40 -0400

    add DB helper to return draft participants array

--- Server/DB/drafts.js
+++ Server/DB/drafts.js
@@ -8,10 +8,27 @@ export async function getDraftsCollection() {
   return { draftsCollection, mongoClient };
 }
 
-export async function getDraftParticipantsCollection({ draftId }) {
+export async function getDraftParticipantsCollection(draftId) {
   const draftParticipantsCollection = mongoClient
     .db('valorant-draft-db')
     .collection(`draft-participants-${draftId}`);
 
-  return { draftParticipantsCollection, mongoClient };
+  return {
+    draftParticipantsCollection,
+    mongoClient
+  };
+}
+
+export async function getDraftParticipants(draftId) {
+  const { draftParticipantsCollection } = await getDraftParticipantsCollection(
+    draftId
+  );
+
+  if (!draftParticipantsCollection) {
+    return null;
+  }
+
+  const participants = await draftParticipantsCollection.find({}).toArray();
+
+  return participants;
 }
